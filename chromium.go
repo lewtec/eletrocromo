9382b0185dc8d1607c65c8a1459a7b622fed2b76
lucasew <lucas59356@gmail.com>
2025-11-28 13:57:35 -0300

    opens chromium
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- chromium.go
+++ chromium.go
@@ -1,7 +1,54 @@
 package eletrocromo
 
+import (
+	"errors"
+	"fmt"
+	"os/exec"
+
+	"github.com/jasonlovesdoggo/gopen"
+)
+
+var chromiumLikes = []string{
+	"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe", // we hate it but we can count it's there
+	"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+	"/Applications/Chromium.app/Contents/MacOS/Chromium",
+	"/Applications/Brave Browser.app/Contents/MacOS/Brave Browser",
+	"/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge",
+	"/Applications/Vivaldi.app/Contents/MacOS/Vivaldi",
+	"/Applications/Opera.app/Contents/MacOS/Opera",
+	"msedge",
+	"brave",
+	"vivaldi",
+	"opera",
+	"chromium",
+	"chrome",
+	"google-chrome",
+	"google-chrome-stable",
+	"chromium",
+	"chromium-browser",
+}
+
+var ErrNoChromium = errors.New("no chromium detected")
+
+func GetChromium() (string, error) {
+	for _, ch := range chromiumLikes {
+		path, err := exec.LookPath(ch)
+		if errors.Is(err, exec.ErrNotFound) {
+			continue
+		}
+		if err != nil {
+			continue
+		}
+		return path, nil
+	}
+	return "", ErrNoChromium
+}
+
 func LaunchChromium(url string) error {
-	// TODO: implement --app
-	// TODO: implement xdg-open like as fallback
-	return nil
+	chromium, err := GetChromium()
+	if errors.Is(err, ErrNoChromium) {
+		return gopen.Open(url)
+	}
+	cmd := exec.Command(chromium, fmt.Sprintf("--app=%s", url))
+	return cmd.Start()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/lewtec/eletrocromo
 
 go 1.25.4
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/google/uuid v1.6.0
+	github.com/jasonlovesdoggo/gopen v0.0.0-20250130105607-39c98c645030
+)
